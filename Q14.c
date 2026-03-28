106a1032d22c41061dbe7af879832ad5c89fa898
Vinayak Dubey <dvinayak139@gmail.com>
2026-03-28 22:06:29 +0530

    Add files via upload

---
+++ Q14.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    // Input matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int isIdentity = 1;
+
+    // Check identity matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j) {
+                if(matrix[i][j] != 1) {
+                    isIdentity = 0;
+                    break;
+                }
+            } else {
+                if(matrix[i][j] != 0) {
+                    isIdentity = 0;
+                    break;
+                }
+            }
+        }
+        if(isIdentity == 0) {
+            break;
+        }
+    }
+
+    // Output
+    if(isIdentity)
+        printf("Identity Matrix\n");
+    else
+        printf("Not an Identity Matrix\n");
+
+    return 0;
+}
